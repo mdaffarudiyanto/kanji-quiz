0c57d42d885725a21a3b85064c75f42aac9a9b32
Chxppa9 <rafiandrianto97@gmail.com>
2023-07-17 02:02:46 +0900

    Test design

---
+++ kanji_react/src/CartoonyTextAnimation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./CartoonyTextAnimation.css";
+
+function CartoonyTextAnimation() {
+  return (
+    <div className="App">
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <text className="cartoony-text" x="50%" y="50%">
+          Kanji Quiz
+        </text>
+      </svg>
+    </div>
+  );
+}
+
+export default CartoonyTextAnimation;
